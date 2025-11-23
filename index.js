7c37eab43e74f95a4c843d2ea0bc8ce6f9d92b86
root <dhkim95@hotmail.com>
2025-11-23 08:58:47 +0000

    fix

--- index.js
+++ index.js
@@ -28,37 +28,57 @@ async function main() {
   let c = [];
 
   for (let i = 0; i < 100; i++) {
-    c.push(1.0);
+    let obj = {};
+    obj.id = i;
+    obj.val = 1.0;
+    c.push(obj);
   }
+
+  let cL = c.length;
 
   console.log({ c });
 
   for (let i = 0; i < 10000000000000; i++) {
     c = c.map((e) => {
-      let result = 0;
       let r = Math.random();
 
-      let m = 1.02;
-      let m2 = 1.02;
+      let sigma = 0.01;
+      let mu = 0.00005;
 
       if (r >= 0.5) {
-        result = e * m;
+        e.val += e.val * mu + e.val * sigma;
       } else {
-        result = e / m2;
+        e.val += e.val * mu - e.val * sigma;
       }
-      return result;
+
+      return e;
     });
 
-    if (i % 1000000 == 0) {
-      // c.sort((a, b) => b - a);
-      let avg = calculateAverage(c);
-      c = c.map((e) => e / avg);
+    if (i % 4000000 == 0) {
+      c = c.filter((e) => e.val > Math.pow(1.0, 0.0));
 
-      let max = Math.max(...c);
-      let mid = calculateMedian(c);
-      avg = calculateAverage(c);
-      console.log({ c, max, mid, avg });
-      // await new Promise((r) => setTimeout(r, 2000));
+      let c2 = c.slice();
+      c2.sort((a, b) => b.val - a.val);
+      c = c2;
+
+      let cVal = c.map((e) => e.val);
+      let max = Math.max(...cVal);
+      let min = Math.min(...cVal);
+      let max2 = c2[1];
+      let mid = calculateMedian(cVal);
+      let avg = calculateAverage(cVal);
+
+      let cT = c.slice(0, 10);
+
+      console.log({ c, max, max2, avg, mid, min });
+      //       await new Promise((r) => setTimeout(r, 2000));
+
+      for (let i = 0; i < 5; i++) {
+        let obj = {};
+        obj.id = cL++;
+        obj.val = 1.0;
+        c.push(obj);
+      }
     }
   }
 }
